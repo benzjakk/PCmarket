9dc37d266b01ec4580cc959a813c1f1d83bfe8aa
benzjakk <benzsport357@gmail.com>
2020-07-24 13:34:50 +0700

    testNewDesign

--- pcmarket/src/App.js
+++ pcmarket/src/App.js
@@ -21,51 +21,62 @@ import "./App.css";
 function App() {
   return (
     <Router>
-      <div className="App">
-        <NavTab />
+      <div>
+        <div
+          style={{
+            display: "none",
+            position: "fixed",
+            backgroundColor: "white",
+            width: "100vw",
+            height: "30px",
+          }}
+        ></div>
+        <div className="App">
+          <NavTab />
 
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          <Route path="/cpu">
-            <CPUResult />
-          </Route>
-          <Route path="/displaycard">
-            <GPUResult />
-          </Route>
-          <Route path="/mainboard">
-            <MBResult />
-          </Route>
-          <Route path="/ram">
-            <RamResult />
-          </Route>
-          <Route path="/storage">
-            <StorageResult />
-          </Route>
-          <Route path="/cooling">
-            <CoolingResult />
-          </Route>
-          <Route path="/psu">
-            <PSUResult />
-          </Route>
-          <Route path="/case">
-            <CaseResult />
-          </Route>
-          <Route path="/gaminggear">
-            <GamingGearResult />
-          </Route>
-          <Route path="/network">
-            <NetworkResult />
-          </Route>
-          <Route component={ItemResult} exact path="/item/:id" />
-          <Route path="/register">
-            <RegisterForm />
-          </Route>
-          <Route path="/upload">
-            <UploadForm />
-          </Route>
-        </Switch>
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route path="/cpu">
+              <CPUResult />
+            </Route>
+            <Route path="/displaycard">
+              <GPUResult />
+            </Route>
+            <Route path="/mainboard">
+              <MBResult />
+            </Route>
+            <Route path="/ram">
+              <RamResult />
+            </Route>
+            <Route path="/storage">
+              <StorageResult />
+            </Route>
+            <Route path="/cooling">
+              <CoolingResult />
+            </Route>
+            <Route path="/psu">
+              <PSUResult />
+            </Route>
+            <Route path="/case">
+              <CaseResult />
+            </Route>
+            <Route path="/gaminggear">
+              <GamingGearResult />
+            </Route>
+            <Route path="/network">
+              <NetworkResult />
+            </Route>
+            <Route component={ItemResult} exact path="/item/:id" />
+            <Route path="/register">
+              <RegisterForm />
+            </Route>
+            <Route path="/upload">
+              <UploadForm />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </Router>
   );
